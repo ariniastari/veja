e3c0305561d3500a043566b38950fd7382acb149
Bondan Herumurti <bherumurti@gmail.com>
2014-12-20 23:00:35 +0700

    update kerjaan jam 11
    Signed-off-by: Bondan Herumurti <bherumurti@gmail.com>

--- wp-content/themes/veja/upload-page.php
+++ wp-content/themes/veja/upload-page.php
@@ -17,42 +17,16 @@ Template Name: Upload Page Template
         </div>
       </div>
       <div class='row'>
-        <form role='form'>
-          <div class='col-sm-5 col-sm-offset-1 text-center'>
-            <img id='target' src='assets/images/upload.png'>
-            <span class='btn btn-pink btn-sm btn-file'>
-              Click here to choose your file
-              <input id='imgInp' type='file'>
-            </span>
-          </div>
-          <div class='col-sm-5'>
-            <div class='form-group'>
-              <input class='form-control' id='name' placeholder='Name' style='margin-top: 10px;' type='text'>
-            </div>
-            <div class='form-group'>
-              <input class='form-control' id='email' placeholder='E-mail' type='email'>
-            </div>
-            <div class='form-group'>
-              <input class='form-control' id='name' placeholder='Nomor HP' type='text'>
-            </div>
-            <div class='form-group'>
-              <textarea class='form-control' placeholder='Alamat' rows='7'></textarea>
-            </div>
-          </div>
-          <div class='col-sm-10 col-sm-offset-1'>
-            <div class='form-group'>
-              <label for='name'>
-                <h3>Tulis Caption Anda</h3>
-              </label>
-              <textarea class='form-control' rows='3'></textarea>
-            </div>
-          </div>
-          <div class='col-sm-12 text-center'>
-            <button class='btn btn-danger' type='submit'>Upload</button>
-          </div>
-        </form>
+        <?php echo do_shortcode('[ngg_uploader id=1]'); ?>
       </div>
     </div>
   </div>
 </div>
+<div id="primary" class="site-content">
+  <div id="content" role="main">
+
+  <?php the_content(); ?>
+
+  </div><!-- #content -->
+</div><!-- #primary -->
 <?php get_footer(); ?>
