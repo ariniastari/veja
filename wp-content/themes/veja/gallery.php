cf8eba7f1a24c0b154eda8588e742b8ea1fe67c4
Bondan Herumurti <bherumurti@gmail.com>
2014-12-21 16:34:38 +0700

    updated  work on galleries
    done
    
    Signed-off-by: Bondan Herumurti <bherumurti@gmail.com>

---
+++ wp-content/themes/veja/gallery.php
@@ -0,0 +1,151 @@
+<?php
+/* 
+Template Name: Gallery Template
+*/
+?>
+
+<?php get_header(); ?>
+<?php //echo do_shortcode('[ngg_images gallery_ids="1" display_type="photocrati-nextgen_basic_thumbnails"]');?>
+    <div class='container-fluid content'>
+      <div class='row'>
+        <div class='col-md-8 col-md-offset-2 white'>
+          <div class='row'>
+            <div class='col-sm-5 text-right'>
+              <h1>Gallery</h1>
+            </div>
+            <div class='col-sm-6'>
+              <h4 class='text-center'>Testimonial Campaign</h4>
+              <h5 class='text-left'>"Apa Jadinya Indonesia Tanpa Asisten Rumah Tangga?"</h5>
+            </div>
+          </div>
+          <?php
+    // Start the loop.
+    while ( have_posts() ) : the_post();
+
+      // Include the page content template.
+      get_template_part( 'content', 'page' );
+
+    // End the loop.
+    endwhile;
+    ?>
+          <!-- <div class='row gallery'>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-3'>
+              <div class='gallery-image'>
+                <div class='tile'>
+                  <img class='user' src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/img.jpg'>
+                  <div class='caption'>
+                    <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> -->
+          <div class='row'>
+            <div class='col-sm-6'>
+              <nav>
+                <ul class='pagination'>
+                  <li class='first'>
+                    <a href='#'>PAGE</a>
+                  </li>
+                  <li>
+                    <a href='#'>1</a>
+                  </li>
+                  <li>
+                    <a href='#'>2</a>
+                  </li>
+                  <li>
+                    <a href='#'>3</a>
+                  </li>
+                  <li>
+                    <a href='#'>4</a>
+                  </li>
+                  <li>
+                    <a href='#'>5</a>
+                  </li>
+                  <li>
+                    <a href='#'>
+                      <span aria-hidden='true'>&raquo;</span>
+                      <span class='sr-only'>Next</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div class='col-sm-6 text-right'>
+              <a class='btn btn-danger' href='#'>Join the Campaign</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php get_footer(); ?>
