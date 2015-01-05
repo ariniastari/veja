301b14a2573dcdfff35cdcf2e07606082c983d43
ariniastari <astariarini@gmail.com>
2015-01-05 20:13:34 +0700

    copy how to popup

--- wp-content/themes/veja/footer.php
+++ wp-content/themes/veja/footer.php
@@ -25,12 +25,12 @@
       <div class='modal-dialog'>
         <div class='modal-content'>
           <div class='modal-body'>
-            <h4 class='text-center'>Testimonial Campaign</h4>
+            <h4 class='text-center'>Testimoni Campaign</h4>
             <h5 class='text-center'>"Apa Jadinya Indonesia Tanpa Asisten Rumah Tangga?"</h5>
             <img src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pop-up.jpg'>
-            <p>Sed accumsan vulputate libero in convallis. Nunc eget dui lacus. Quisque lobortis mi ac erat pulvinar commodo. Ut dapibus in odio vitae consectetur.</p>
+            <p>Bagi Anda para Supermama, pastinya hal tersebut tidak menjadi halangan. Tunjukkan sisi Supermama Anda dengan menjadi bagian dalam kumpulan Supermama Indonesia bersama Veja.</p>
             <div class='text-center'>
-              <a class='btn btn-danger' href='<?php echo esc_url( get_permalink( get_page_by_title( 'How To' ) ) ); ?>'>How To</a>
+              <a class='btn btn-danger' href='<?php echo esc_url( get_permalink( get_page_by_title( 'How To' ) ) ); ?>'>Cara Ikutan</a>
             </div>
           </div>
         </div>
