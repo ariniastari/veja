d8e70c0924b6efe63cbad6b99a979b5c00d637c4
ariniastari <astariarini@gmail.com>
2015-01-05 20:16:00 +0700

    how to copy

--- wp-content/themes/veja/how_to.php
+++ wp-content/themes/veja/how_to.php
@@ -13,20 +13,20 @@ Template Name: How To Template
               <h1>Cara Ikutan</h1>
             </div>
             <div class='col-sm-6'>
-              <h4>Testimonial Campaign</h4>
+              <h4>Testimoni Campaign</h4>
               <h5>"Apa Jadinya Indonesia Tanpa Asisten Rumah Tangga?"</h5>
             </div>
           </div>
           <div class='row'>
             <div class='col-sm-6'>
               <div class='sprites upload'></div>
-              <h3>Upload Foto Anda via Website</h3>
-              <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris. Aenean facilisis tellus quis condimentum ultricies.</p>
+              <h3>UPLOAD FOTO KREATIF MAMA</h3>
+              <p>Upload pose kreatif Supermama Anda bersama Veja melalui Supermama.veja.co.id. Tunjukkan bagaimana Veja mendukung mama dalam menjadi Supermama bagi keluarga Anda</p>
             </div>
             <div class='col-sm-6'>
               <div class='sprites caption'></div>
-              <h3>Tulis Caption Anda</h3>
-              <p>Cras ut mi consectetur, interdum sapien at, fringilla mauris. Aenean facilisis tellus quis condimentum ultricies.</p>
+              <h3>BAGIKAN TESTIMONI MAMA</h3>
+              <p>Jangan lupa untuk menuliskan testimoni atau cerita menarik mama sebagai Supermama karena Veja. Hal ini akan menjadi salah satu aspek penilaian yang tidak kalah penting.</p>
             </div>
           </div>
           <div class='row'>
