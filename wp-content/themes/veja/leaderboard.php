de88e246189d92ce9c9d0d74b63dc47c9f7fed33
ariniastari <astariarini@gmail.com>
2015-01-09 08:31:39 +0700

    add leaderboard

---
+++ wp-content/themes/veja/leaderboard.php
@@ -0,0 +1,81 @@
+<?php
+/* 
+Template Name: Leaderboard Template
+*/
+?>
+
+<?php get_header(); ?>
+	<div class='container-fluid content'>
+    <div class='row'>
+      <div class='col-md-8 col-md-offset-2 white'>
+        <h1 class='text-center'>Leaderboard</h1>
+        <div class="table-responsive">
+
+          <!-- Table -->
+          <table class="table">
+            <tr>
+              <td>1.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>2.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>3.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>4.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>5.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>6.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>7.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>8.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>9.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+            <tr>
+              <td>10.</td>
+              <td>Ayu Dewi</td>
+              <td>0812xxxx1900</td>
+              <td>230 points</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php get_footer(); ?>
