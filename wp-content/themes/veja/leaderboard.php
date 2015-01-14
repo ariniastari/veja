10b7c48d9579d374727c448c3896e6f5640a288c
ariniastari <astariarini@gmail.com>
2015-01-15 06:41:09 +0700

    add search form in leaderboard

--- wp-content/themes/veja/leaderboard.php
+++ wp-content/themes/veja/leaderboard.php
@@ -8,72 +8,88 @@ Template Name: Leaderboard Template
 	<div class='container-fluid content'>
     <div class='row'>
       <div class='col-md-8 col-md-offset-2 white'>
-        <h1 class='text-center'>Leaderboard</h1>
-        <div class="table-responsive">
+        <div class="col-md-6">
+          <h1>Leaderboard</h1>
+        </div>
+        <div class="col-md-6">
+          <div class="form-search">
+            <form>
+              <div class="inner-addon right-addon">
+                <i class="glyphicon glyphicon-search pink"></i>
+                <input type="text" class="form-control" placeholder="Cari nama Mama disini" />
+                <input type="submit" value="Go" class="hidden" />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="col-md-12">
+          <p class="search-result"> Hasil pencarian untuk <span class="blue">"Jane Smith"</span></p>
+          <div class="table-responsive">
 
-          <!-- Table -->
-          <table class="table">
-            <tr>
-              <td>1.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>2.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>3.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>4.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>5.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>6.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>7.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>8.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>9.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-            <tr>
-              <td>10.</td>
-              <td>Ayu Dewi</td>
-              <td>0812xxxx1900</td>
-              <td>230 points</td>
-            </tr>
-          </table>
+            <!-- Table -->
+            <table class="table">
+              <tr>
+                <td>1.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>2.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>3.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>4.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>5.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>6.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>7.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>8.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>9.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+              <tr>
+                <td>10.</td>
+                <td>Ayu Dewi</td>
+                <td>0812xxxx1900</td>
+                <td>230 points</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>
